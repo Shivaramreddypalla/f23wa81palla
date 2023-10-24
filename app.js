d624720a4159650d8c3aff22460f44687b9c00c0
Shivaram Reddy <shivaramreddy.91@gmail.com>
2023-10-23 22:48:56 -0500

    new endpoint

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/mydata');
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -20,8 +20,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/mydata', mydataRouter);
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/bonus', bonusRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,6 +38,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
 
 module.exports = app;
--- routes/mydata.js
+++ routes/mydata.js
@@ -6,5 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('mydata', { title: 'Shivaram Reddy Palla' });
 });
 
-
 module.exports = router;
