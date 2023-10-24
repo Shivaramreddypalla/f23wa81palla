7e06e2195163c5510e815767b0320451ee43f055
Shivaram Reddy <shivaramreddy.91@gmail.com>
2023-10-23 22:30:40 -0500

    made changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/mydata', mydataRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
