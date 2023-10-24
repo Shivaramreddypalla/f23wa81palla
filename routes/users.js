1416fed3ea15e0950a6ef00afe7c8aa1f630d229
Shivaram Reddy <shivaramreddy.91@gmail.com>
2023-10-24 17:40:24 -0500

    updated code

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,14 @@
 var express = require('express');
-
 var router = express.Router();
-
-var sum = 0;  // Initialize sum to 0
-
-var more = 0; // Initialize more to 0
-
-
-/* GET users listing. */
+var sum = 0;
+var more = 0;
 
 router.get('/', function(req, res, next) {
 
-  more += 1;  // Add 1 to more
-
-  sum += more; // Add the updated value of more to sum
-
-
-  res.send(`Sum is: ${sum}`); // Send the response with the updated sum
+  more += 1; 
+  sum += more;
+  res.send(`Sum is: ${sum}`);
 
 });
-
 
 module.exports = router;
