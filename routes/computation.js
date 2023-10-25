c5f660dc4a6786719d4bfa8bade14e95dcd7125f
Shivaram Reddy <shivaramreddy.91@gmail.com>
2023-10-25 17:13:56 -0500

    updated bonus

--- routes/computation.js
+++ routes/computation.js
@@ -1,25 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-var rand1 = Math.random() * 100;
-var rand2 = Math.random() * 200;
+var random = Math.floor(Math.random() * 100);
+var rand = Math.floor(Math.random() * 10);
 var num1 = 0;
 var num2 = 0;
 var num3 = 0;
 
 router.get('/', function (req, res, next) {
 
-  num1 = Math.log(rand1);
-  num2 = Math.log(rand2);
-  num3 = Math.log(rand1);
+  num1 = Math.imul(random,rand);
+  num2 = Math.log(random);
+  num3 = Math.log10(random);
 
-  res.send(`The Random numbers are ${rand1} and ${rand2}
-
-    <br> <br> log applied to ${rand1} is ${num1}
-    <br> <br> log applied to ${rand2} is ${num2}
-    <br> <br> log applied to ${rand1} is ${num3}`);
+  res.send(`imul applied to (${random}, ${rand}) is ${num1}
+    <br> <br> log applied to ${random} is ${num2}
+    <br> <br> log10 applied to ${random} is ${num3}`);
 
 });
 
- 
 module.exports = router
